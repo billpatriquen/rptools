b5f6616f7cf688a84f863fb06282bdce1d7b7831
Bill Patriquen <billpatriquen@gmail.com>
2019-01-10 13:15:42 -0400

    Encounter Tables
    Initial setup for the Encounter Tables app

---
+++ encounter-tables/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+
+  win = new BrowserWindow({ 
+    width: 800, 
+    height: 600,
+    backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/assets/logo.png` });
+
+  win.loadURL(`file://${__dirname}/dist/angular-tour-of-heroes/index.html`)
+
+  //win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+
+  if (process.plantform !== 'darwin') {
+    app.quit();
+  }
+
+});
+
+app.on('activate', () => {
+
+  if (win === null) {
+    createWindow();
+  }
+
+});
